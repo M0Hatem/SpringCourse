a67570f6a9c39c560c44658edf24503a32a3df51
M16 <moh.medh.tem1@gmail.com>
2024-08-13 14:11:20 +0300

    initial Commit

--- firstspringwebapp/src/main/java/com/firstspringwebapp/FirstSpringWebApplication.java
+++ firstspringwebapp/src/main/java/com/firstspringwebapp/FirstSpringWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstspringwebappApplication {
+public class FirstSpringWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstspringwebappApplication.class, args);
+        SpringApplication.run(FirstSpringWebApplication.class, args);
     }
 
 }
