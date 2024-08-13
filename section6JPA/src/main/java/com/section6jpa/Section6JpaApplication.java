758cff6956cb9a7c449cc06210642512a9a4c9d9
M16 <moh.medh.tem1@gmail.com>
2024-08-13 10:02:38 +0300

    initial commit

---
+++ section6JPA/src/main/java/com/section6jpa/Section6JpaApplication.java
@@ -0,0 +1,13 @@
+package com.section6jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section6JpaApplication.class, args);
+    }
+
+}
