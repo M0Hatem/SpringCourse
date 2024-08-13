3446f112577629147077b9e68400c3385d5e1670
M16 <moh.medh.tem1@gmail.com>
2024-08-13 14:10:00 +0300

    initial Commit

---
+++ firstspringwebapp/src/main/java/com/firstspringwebapp/FirstspringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.firstspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstspringwebappApplication.class, args);
+    }
+
+}
---
+++ firstspringwebapp/src/test/java/com/firstspringwebapp/FirstspringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
