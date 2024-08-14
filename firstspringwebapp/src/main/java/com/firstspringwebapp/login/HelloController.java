2b56a51738037c522ab12b8a79b45cf527ae27a8
M16 <moh.medh.tem1@gmail.com>
2024-08-14 09:46:47 +0300

    working with views

--- firstspringwebapp/src/main/java/com/firstspringwebapp/login/HelloController.java
+++ firstspringwebapp/src/main/java/com/firstspringwebapp/login/HelloController.java
@@ -1,4 +1,4 @@
-package com.firstspringwebapp.controller;
+package com.firstspringwebapp.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
